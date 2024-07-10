c5c1b479173b0a0b6e3f965722bc745ac112f029
Anthesm <akganthesm@gmail.com>
2024-07-10 21:24:32 +0530

    updated folder structure

---
+++ src/screens/InitialScreens.jsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const InitialScreens = () => {
+  return (
+    <View>
+      <Text>Beginner</Text>
+      <Text>INterMidiate</Text>
+      <Text>Advance</Text>
+    </View>
+  );
+};
+
+export default InitialScreens;
---
+++ src/usables/ScreenCompo.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+import Button from './Button';
+import {GlobalStyles, CommonStyles} from '../styles/globalcss';
+
+const ReusableComponent = ({
+  title,
+  text,
+  buttonText,
+  onButtonPress,
+  imageSource,
+  imageStyle,
+}) => {
+  return (
+    <View style={GlobalStyles.container}>
+      <View style={GlobalStyles.centeredContainer}>
+        <Image source={imageSource} style={[styles.image, imageStyle]} />
+        <Text style={CommonStyles.header}>{title}</Text>
+        <Text style={CommonStyles.text}>{text}</Text>
+        <Button
+          title={buttonText}
+          onPress={onButtonPress}
+          style={CommonStyles.button}
+          textStyle={CommonStyles.buttonText}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
+
+ReusableComponent.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  buttonText: PropTypes.string,
+  onButtonPress: PropTypes.func,
+  imageSource: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  imageStyle: PropTypes.object,
+};
+
+ReusableComponent.defaultProps = {
+  title: 'Default Title',
+  text: 'Default Text',
+  buttonText: 'Click Me',
+  onButtonPress: () => {
+    console.log('Button Pressed');
+  },
+  imageSource: require('../../assets/frontLogo.png'), // Default image
+  imageStyle: {},
+};
+
+export default ReusableComponent;
